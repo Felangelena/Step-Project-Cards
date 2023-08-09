c09a98c05ce4c574a3a6ce4a139c5b41b615929f
Konstantyn Danylenko <k.danilenko1982@gmail.com>
2023-08-10 00:29:13 +0300

    Описание ваших изменений

--- src/js/exit.js
+++ src/js/exit.js
@@ -1,9 +1,16 @@
 
 const enterButton = document.getElementById('enterButton');
-
 const formElement = document.querySelector('.form-autoris form');
-
+const overlayElement = document.querySelector('.overlay'); 
 
 enterButton.addEventListener('click', function() {
-  formElement.style.display = 'block'; 
+  formElement.style.display = 'block';
+  overlayElement.style.display = 'block'; 
+});
+
+overlayElement.addEventListener('click', function(event) {
+  if (event.target === overlayElement) {
+    formElement.style.display = 'none';
+    overlayElement.style.display = 'none'; 
+  }
 });
