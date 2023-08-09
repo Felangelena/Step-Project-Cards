a25d6677b0e6c3e7ede673dffc71265cb2b60581
Konstantyn Danylenko <k.danilenko1982@gmail.com>
2023-08-09 04:53:06 +0300

    main commit

---
+++ src/js/exit.js
@@ -0,0 +1,9 @@
+
+const enterButton = document.getElementById('enterButton');
+
+const formElement = document.querySelector('.form-autoris form');
+
+
+enterButton.addEventListener('click', function() {
+  formElement.style.display = 'block'; 
+});
