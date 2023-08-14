f6a1b21f7a7533de9fd31f4ab87c2731835ceb0c
Konstantyn Danylenko <k.danilenko1982@gmail.com>
2023-08-14 04:51:19 +0300

    kosss commit

--- src/js/login.js
+++ src/js/login.js
@@ -78,6 +78,11 @@ overlayElement.addEventListener('click', function(event) {
 
 
 
+
+
+
+
+
 // второй єтап
 
 
@@ -109,11 +114,13 @@ const metaInput = document.createElement('input');
 metaInput.type = 'text';
 metaInput.classList.add('form-control');
 metaInput.placeholder = 'Мета візиту';
+metaInput.required = true; // Добавляем required к полю
 
 const descriptionInput = document.createElement('input');
 descriptionInput.type = 'text';
 descriptionInput.classList.add('form-control');
 descriptionInput.placeholder = 'Короткий опис візиту';
+descriptionInput.required = true; // Добавляем required к полю
 
 const urgencySelect = document.createElement('select');
 urgencySelect.classList.add('form-control');
@@ -127,7 +134,36 @@ const fullNameInput = document.createElement('input');
 fullNameInput.type = 'text';
 fullNameInput.classList.add('form-control');
 fullNameInput.placeholder = 'ПІБ';
+fullNameInput.required = true;
 
+
+const dateInputLabel = document.createElement('label');
+dateInputLabel.textContent = 'Дата і час візиту'; // Текст надписи
+dateInputLabel.classList.add('input-label'); // Добавляем класс для стилизации
+
+// Создаем элемент input для даты и времени
+const dateInput = document.createElement('input');
+dateInput.type = 'datetime-local';
+dateInput.classList.add('form-control');
+dateInput.required = true;
+
+
+
+
+
+// Кнопка "Створити"
+const createCardButton = document.createElement('button');
+createCardButton.type = 'submit';
+createCardButton.classList.add('btn', 'btn-sm');
+createCardButton.id = 'addCardSubmitButton';
+createCardButton.textContent = 'Створити';
+
+// Кнопка "Закрити"
+const closeButton = document.createElement('button');
+closeButton.type = 'button';
+closeButton.classList.add('btn', 'btn-sm');
+closeButton.id = 'ExitAddCardButton';
+closeButton.textContent = 'Закрити';
 
 
 createButton.addEventListener('click', function(event) {
@@ -135,6 +171,7 @@ createButton.addEventListener('click', function(event) {
 
     openModal();
 });
+
 
 function openModal() {
     formElement.classList.add('Modal');
@@ -147,17 +184,40 @@ function openModal() {
     });
 
     doctorSelect.style.display = 'block';
+    extraFieldsContainer.style.display = 'block';
+    
+    
+    
 
+    
     formElement.appendChild(metaInput);
     formElement.appendChild(descriptionInput);
     formElement.appendChild(urgencySelect);
     formElement.appendChild(fullNameInput);
-}
+    formElement.appendChild(dateInputLabel);
+    formElement.appendChild(dateInput);
+    formElement.appendChild(createCardButton);
+    formElement.appendChild(closeButton);}
+    
+
+
+
 
 function closeModal() {
+    formElement.reset()
     formElement.classList.remove('Modal');
     overlay.style.display = 'none';
     formElement.style.display = 'none';
+
+    // Сбрасываем значения всех полей в начальное состояние
+    dateInput.value = ''; 
+    metaInput.value = ''; 
+    descriptionInput.value = ''; 
+    urgencySelect.value = 'normal'; 
+    fullNameInput.value = ''; 
+    
+    // Убираем дополнительные поля в зависимости от выбранного доктора
+    extraFieldsContainer.innerHTML = '';
 
     const formGroups = formElement.querySelectorAll('.form-group');
     formGroups.forEach(group => {
@@ -165,14 +225,152 @@ function closeModal() {
     });
 
     doctorSelect.style.display = 'none';
+    extraFieldsContainer.style.display = 'none';
+    
 
+    formElement.removeChild(dateInput);
     formElement.removeChild(metaInput);
     formElement.removeChild(descriptionInput);
     formElement.removeChild(urgencySelect);
     formElement.removeChild(fullNameInput);
+    formElement.removeChild(createCardButton);
+    formElement.removeChild(closeButton);
+    formElement.removeChild(dateInputLabel);
+
+
+
+    
+}
+
+
+// Обработчик для кнопки "Закрити"
+closeButton.addEventListener('click', closeModal);
+
+exitButton.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
+
+const extraFieldsContainer = document.querySelector('.extra-fields-container');
+
+
+function applyDoctorClass(selectedDoctor) {
+    formElement.classList.remove('VisitDentist', 'VisitCardiologist', 'VisitTherapist');
+    formElement.classList.add(selectedDoctor);
+    extraFieldsContainer.innerHTML = ''; // Очищаем контейнер перед добавлением новых полей
+}
+
+doctorSelect.addEventListener('change', function () {
+    const selectedDoctor = doctorSelect.value;
+
+    switch (selectedDoctor) {
+        case 'Dentist':
+            applyDoctorClass('VisitDentist');
+            addDentistFields();
+            break;
+        case 'Cardiologist':
+            applyDoctorClass('VisitCardiologist');
+            addCardiologistFields();
+            break;
+        case 'Therapist':
+            applyDoctorClass('VisitTherapist');
+            addTherapistFields();
+            break;
+        default:
+            applyDoctorClass('');
+            extraFieldsContainer.innerHTML = ''; // Скрываем дополнительные поля при выборе "Всі візити"
+    }
+});
+
+// function addDentistFields() {
+
+//     const lastVisitInput = document.createElement('input');
+//     lastVisitInput.type = 'date';
+//     lastVisitInput.classList.add('form-control');
+//     lastVisitInput.placeholder = 'Дата останнього візиту';
+//     lastVisitInput.required = true; // Добавляем required к полю - обязательное заполнение!
+//     extraFieldsContainer.appendChild(lastVisitInput);
+// }
+function addDentistFields() {
+    const lastVisitLabel = document.createElement('label');
+    lastVisitLabel.textContent = 'Дата останнього візиту';
+    lastVisitLabel.classList.add('input-label');
+
+    const lastVisitInput = document.createElement('input');
+    lastVisitInput.type = 'date';
+    lastVisitInput.classList.add('form-control');
+    lastVisitInput.required = true;
+
+    extraFieldsContainer.appendChild(lastVisitLabel);
+    extraFieldsContainer.appendChild(lastVisitInput);
 }
 
 
 
-exitButton.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+
+function addCardiologistFields() {
+    const bloodPressureInput = document.createElement('input');
+    bloodPressureInput.type = 'text';
+    bloodPressureInput.classList.add('form-control');
+    bloodPressureInput.placeholder = 'Звичайний тиск';
+    bloodPressureInput.required = true; // Добавляем required к полю
+    extraFieldsContainer.appendChild(bloodPressureInput);
+
+    const bmiInput = document.createElement('input');
+    bmiInput.type = 'text';
+    bmiInput.classList.add('form-control');
+    bmiInput.placeholder = 'Індекс маси тіла';
+    bmiInput.required = true; // Добавляем required к полю
+    extraFieldsContainer.appendChild(bmiInput);
+
+    const cardiacDiseasesInput = document.createElement('input');
+    cardiacDiseasesInput.type = 'text';
+    cardiacDiseasesInput.classList.add('form-control');
+    cardiacDiseasesInput.placeholder = 'Перенесені захворювання ССС';
+    cardiacDiseasesInput.required = true; 
+    extraFieldsContainer.appendChild(cardiacDiseasesInput);
+
+    const ageInput = document.createElement('input');
+    ageInput.type = 'text';
+    ageInput.classList.add('form-control');
+    ageInput.placeholder = 'Вік';
+    ageInput.required = true;
+    ageInput.addEventListener('input', function() {
+        // Проверяем, что введенное значение состоит только из цифр и находится в диапазоне от 1 до 120
+        const inputValue = ageInput.value;
+        if (/^[1-9][0-9]?$|^120$/.test(inputValue)) {
+            ageInput.setCustomValidity(''); // Валидация успешна, сбрасываем сообщение об ошибке
+        } else {
+            ageInput.setCustomValidity('Введіть коректне значення віку (від 1 до 120)'); // Не соответствует условиям валидации
+        }
+    });
+    extraFieldsContainer.appendChild(ageInput);
+}
+
+function addTherapistFields() {
+    const ageInput = document.createElement('input');
+    ageInput.type = 'text';
+    ageInput.classList.add('form-control');
+    ageInput.placeholder = 'Вік';
+    ageInput.required = true;
+    ageInput.addEventListener('input', function() {
+        // Проверяем, что введенное значение состоит только из цифр и находится в диапазоне от 1 до 120
+        const inputValue = ageInput.value;
+        if (/^[1-9][0-9]?$|^120$/.test(inputValue)) {
+            ageInput.setCustomValidity(''); // Валидация успешна, сбрасываем сообщение об ошибке
+        } else {
+            ageInput.setCustomValidity('Введіть коректне значення віку (від 1 до 120)'); // Не соответствует условиям валидации
+        }
+    });
+    extraFieldsContainer.appendChild(ageInput);
+}
+
+createCardButton.addEventListener('click', function(event) {
+    if (formElement.classList.contains('Modal') && doctorSelect.value === '') {
+        event.preventDefault();
+        alert('Выберите врача');
+    }
+    // Остальной код для создания визита
+});
+
+
